f8ef92b26e6c07d8f68fe9f85e438a0d4560530d
JH <wlgud5632@naver.com>
2025-01-08 15:26:55 +0900

    테스트종료

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,15 +1,17 @@
 import { createContext, useState } from "react";
 
 const GithubContext = createContext();
-// 깃허브주소오 ㅏ토큰 변수지정
+// 깃허브주소와 토큰 변수지정
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 // 프로바이더가 전역으로 컨텍스트를 적용함
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  // 테스트 유저 리스트
   const fetchUsers = () => {
+    setLoading(true);
     fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
