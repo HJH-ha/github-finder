a98dd3f9297c65dc658b71204107ce052c23ea79
JH <wlgud5632@naver.com>
2025-01-09 11:24:27 +0900

    유저 리포리스트 추가

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,8 +8,13 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 // 프로바이더가 전역으로 컨텍스트를 적용함
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
+  const [repos, setRepos] = useState([]);
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
+  //유저들 클리어
+  const clearUsers = () => {
+    setUsers([]);
+  };
   // 키워드로 유저찾기
   const searchUsers = (text) => {
     setLoading(true);
@@ -44,14 +49,34 @@ export const GithubProvider = ({ children }) => {
       .catch((err) => (window.location = "/notfound"));
   };
 
+  // 유저 공개된 리포 리스트
+  const getUserRepos = (login) => {
+    setLoading(true);
+
+    fetch(`${GITHUB_URL}/users/${login}/repos`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setRepos(data);
+        setLoading(false); // 데이터 로딩완료 false
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <GithubContext.Provider
       value={{
         users,
         user,
         loading,
+        repos,
         searchUsers,
         getUser,
+        clearUsers,
+        getUserRepos,
       }}
     >
       {children}
